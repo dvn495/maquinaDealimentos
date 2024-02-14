f7908d342defa997f8dc79f3ff30cdad95473893
dvn495 <152308538+dvn495@users.noreply.github.com>
2024-02-14 14:39:48 -0500

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -5,4 +5,54 @@
 
 // Escriba un programa que pida al usuario elegir el producto y luego le pida ingresar las monedas hasta alcanzar el monto a pagar. Si el monto ingresado es mayor que el precio del producto, el programa debe entregar las monedas de vuelto, una por una.
 
-let a = prompt("desea c")
+const A = 270
+const B = 340
+const C = 390
+
+let vuelto = 0
+
+let producto = Number(prompt("---Productos---\n  1. Producto A = $270 \n  2. Producto B = $340\n  3. Producto C = $390"))
+
+switch (producto) {
+    case 1:
+        let monedas = 0;
+        let activador = 0;
+        let Isactivesecond = " ";
+        while (activador = 0){
+            let valormonedas = Number(prompt(" Ingrese una moneda (solo de $100-$50-$10)"))
+            if (valormonedas === 100 || valormonedas === 50 ||valormonedas === 10 ) {
+                monedas += valormonedas
+            }else{
+                alert("solo de $100-$50-$10")
+            }
+            Isactivesecond = String(prompt("¿Desea ingresar otra moneda? S(si)//N(no)"));
+            if (Isactivesecond = "n" && monedas < A){
+               activador = 1;
+            }else{
+                alert(`Hacen falta ${A - monedas} monedas para comprar el producto`)
+            }
+            console.log(monedas)
+        }
+        
+        let vuelto = monedas - A;
+
+        while (vuelto > 0) {
+            confirm(`Sus vueltos son de ${vuelto}`)
+            if (vuelto < 100){
+
+                console.log(vuelto)
+            }
+            
+
+        }
+        break;
+    case 2:
+        vuelto = vuelto + 50
+        break;
+    case 3:
+        vuelto = vuelto + 100
+        break;
+    default:
+        alert("No existe el producto")
+}
+
